cc1be51aff0c7ff6b18fb64dff9e216dd1059fa2
Braxtonx9 <braxtonlimited035@gmail.com>
2024-09-29 15:34:16 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -6,11 +6,11 @@ const chalk = require('chalk');
 //owmner v card
 global.ytname = "YT: GlobalTechInfo" //ur yt chanel name
 global.socialm = "GitHub: GlobalTechInfo" //ur github or insta name
-global.location = "Pakistan, Karachi, Karachi" //ur location
+global.location = "Nigeria,Lagos,badagry" //ur location
 
 //new
-global.botname = '© 𝐆𝐋𝐎𝐁𝐀𝐋-𝐌𝐃' //ur bot name
-global.ownernumber = ['923444844060'] //ur owner number, dont add more than one
+global.botname = '© 💀❌💀' //ur bot name
+global.ownernumber = ['2348112312350'] //ur owner number, dont add more than one
 global.ownername = '© GlobalTechInfo' //ur owner name
 global.websitex = "https://www.youtube.com/@GlobalTechInfo"
 global.wagc = "https://whatsapp.com/channel/0029VagJIAr3bbVBCpEkAM07"
@@ -21,7 +21,7 @@ global.packname = "Sticker By"
 global.author = "GlobalTechInfo\n\n+923444844060"
 global.creator = "923444844060@s.whatsapp.net"
 global.xprefix = '.'
-global.premium = ["923444844060"] // Premium User
+global.premium = ["2348112312350"] // Premium User
 
 //channel id
 global.xchannel = {
